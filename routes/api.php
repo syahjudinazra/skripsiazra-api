1846c93f9e9b22406a497ed9cfca3d62b5984e3a
iMin <syahjudinazra@gmail.com>
2023-04-11 17:20:14 +0700

    api update

--- routes/api.php
+++ routes/api.php
@@ -21,14 +21,14 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::middleware(['auth:api'])->group(function () {
-    Route::get('/logout', [AuthController::class, 'logout']);
-    Route::post('/product', [ProductController::class, 'store']);
-    Route::get('/product', [ProductController::class, 'index']);
-    Route::patch('/product/{id}', [ProductController::class, 'update']);
-    Route::delete('/product/{id}', [ProductController::class, 'destroy']);
 });
 
+Route::get('/logout', [AuthController::class, 'logout']);
+Route::post('/product', [ProductController::class, 'store']);
+Route::patch('/product/{id}', [ProductController::class, 'update']);
+Route::delete('/product/{id}', [ProductController::class, 'destroy']);
 
+Route::get('/product', [ProductController::class, 'index']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 Route::get('/product/{id}', [ProductController::class, 'show']);
